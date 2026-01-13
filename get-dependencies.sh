4bd6d00a07f9f17e66c04e4381de5bf8a44a8adf
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-13 12:53:16 -0400

    add pipewire

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm wine patchelf sdl2
+pacman -Syu --noconfirm wine patchelf sdl2 pipewire-audio pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
