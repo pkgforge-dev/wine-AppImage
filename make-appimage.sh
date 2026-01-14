df8abb17093698ce0a11dea07d2aa175c57c4f08
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-14 08:08:13 -0400

    strip windows dlls

--- make-appimage.sh
+++ make-appimage.sh
@@ -64,5 +64,12 @@ fi
 find ./AppDir/lib/ -type f -name '*.a'
 find ./AppDir/lib/ -type f -name '*.a' -delete
 
+# strip windows libs, inspired by alpine linux: 
+# https://gitlab.alpinelinux.org/alpine/aports/-/blob/master/community/wine/APKBUILD
+if [ "$ARCH" = 'x86_64' ]; then
+	x86_64-w64-mingw32-strip -R .comment --strip-unneeded ./AppDir/lib/wine/x86_64-windows/*.dll
+	i686-w64-mingw32-strip   -R .comment --strip-unneeded ./AppDir/lib/wine/i386-windows/*.dll
+fi
+
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
